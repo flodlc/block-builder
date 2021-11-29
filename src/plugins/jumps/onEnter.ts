05d82830b393f15d1ecb8ef2f393931abe5028a4
castroCrea <castro.crea@gmail.com>
2021-11-30 00:03:11 +0100

    ✨ soft break

--- src/plugins/jumps/onEnter.ts
+++ src/plugins/jumps/onEnter.ts
@@ -1,6 +1,10 @@
 import { Editor } from '../../editor/model/Editor';
 import { cutMarkedText } from '../../editor/transaction/MarkedText/cutMarkedText';
 import { TextSelection } from '../../editor/model/Selection';
+import { splitMarkedText } from '../../editor/transaction/MarkedText/splitMarkedText';
+import { minifyMarkedText } from '../../editor/transaction/MarkedText/minifyMarkedText';
+import { getMarkedTextLength } from '../../editor/transaction/MarkedText/getMarkedTextLength';
+import { MarkedNode } from '../../editor/model/types';
 
 export const onEnter = ({
     e,
@@ -9,6 +13,7 @@ export const onEnter = ({
     e: KeyboardEvent;
     editor: Editor;
 }) => {
+    if (softBreak({ e, editor })) return;
     const selection = editor.state.selection as TextSelection;
     const node = editor.state.nodes[selection.nodeId];
     const newNode = editor.schema.text.create();
@@ -45,4 +50,45 @@ export const onEnter = ({
 
     e.preventDefault();
     e.stopPropagation();
+};
+
+const softBreak = ({ e, editor }: { e: KeyboardEvent; editor: Editor }) => {
+    if (e.shiftKey) {
+        const selection = editor.state.selection as TextSelection;
+        const { range } = selection;
+        const node = editor.state.nodes[selection.nodeId];
+        const text = node.text;
+        const textLength = getMarkedTextLength(text ?? []);
+        const isCaretAtTheEnd = textLength === range[1];
+        
+        const softBreak: MarkedNode = {
+            s: '\n ',
+        };
+        
+        /** Avoid css glitch that doesn't goes inline before there is a letter */
+        if (isCaretAtTheEnd) softBreak.s = `${softBreak.s} `;
+        
+        const splittedText = splitMarkedText(text ?? []);
+        const removeIndex = range[1] - range[0];
+        splittedText.splice(range[0] ?? 0, removeIndex, softBreak);
+
+        const newText = minifyMarkedText(splittedText);
+        const newTextLength = range[0] + 1;
+
+        editor
+            .createTransaction()
+            .patch({
+                patch: {
+                    text: newText,
+                },
+                nodeId: selection.nodeId,
+            })
+            .focus(selection.setRange([newTextLength, newTextLength]))
+            .dispatch();
+        e.preventDefault();
+        e.stopPropagation();
+
+        return true;
+    }
+    return false;
 };
